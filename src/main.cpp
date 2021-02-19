ceeeebfec7787ab9a099fad3de77ef227b816afb
Jan H. Krüger <janhkrueger@outlook.com>
2021-02-19 22:43:33 +0000

    Aktualisieren ci-pipelines/.gitlab-ci.yml, ci-pipelines/cleanup.gitlab-ci.yml, ci-pipelines/SAST.gitlab-ci.yml, src/main.cpp Dateien

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : janhkrueger
+// Description : Hello World in C++, tutorial for Gitlab CI/CD Pipeline
+//============================================================================
+
+#include <string>
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
